d40bcc3c2f364ff34f66e045b4915d2eef786cbe
Karel D'Oosterlinck <kareldoosterlinck@gmail.com>
2023-04-20 15:05:52 +0200

    work on more advanced gpt2 finetuning code

--- tasks/icsr_extraction/gpt2/finetune_for_summarization.py
+++ tasks/icsr_extraction/gpt2/finetune_for_summarization.py
@@ -1,4 +1,6 @@
 import torch
+import logging
+import os
 import copy
 from typing import Optional
 from dataclasses import dataclass, field
@@ -17,12 +19,15 @@ from transformers import (
     GPT2LMHeadModel,
     AutoModelForCausalLM,
 )
+import numpy as np
+from transformers.trainer_utils import get_last_checkpoint
 from datasets import load_dataset
 
 
 from sum_data_collator import DataCollatorForSumLanguageModeling
 from sum_dataset import LineByLineSumTextDataset
 
+logger = logging.getLogger(__name__)
 
 @dataclass
 class ModelArguments:
@@ -142,6 +147,22 @@ def finetune():
     # parse args
     parser = HfArgumentParser((ModelArguments, DataArguments, TrainingArguments))
     model_args, data_args, training_args = parser.parse_args_into_dataclasses()
+
+    # Detecting last checkpoint.
+    last_checkpoint = None
+    if os.path.isdir(training_args.output_dir) and training_args.do_train and not training_args.overwrite_output_dir:
+        last_checkpoint = get_last_checkpoint(training_args.output_dir)
+        if last_checkpoint is None and len(os.listdir(training_args.output_dir)) > 0:
+            raise ValueError(
+                f"Output directory ({training_args.output_dir}) already exists and is not empty. "
+                "Use --overwrite_output_dir to overcome."
+            )
+        elif last_checkpoint is not None and training_args.resume_from_checkpoint is None:
+            logger.info(
+                f"Checkpoint detected, resuming training at {last_checkpoint}. To avoid this behavior, change "
+                "the `--output_dir` or add `--overwrite_output_dir` to train from scratch."
+            )
+
     # set seed
     set_seed(training_args.seed)
     # set up model
@@ -181,15 +202,15 @@ def finetune():
     padding = False
     def preprocess_function(examples):
         # tokenize input text and summary
-        text_encoding = tokenizer(examples['abstract'], max_length=data_args.max_source_length, padding=padding, truncation=True, is_split_into_words=False)['input_ids']
-        summary_encoding = tokenizer(examples['target'], max_length=data_args.max_target_length, padding=padding, truncation=True, is_split_into_words=False)['input_ids']
+        text_encoding = tokenizer(examples[data_args.text_column], max_length=data_args.max_source_length, padding=padding, truncation=True, is_split_into_words=False)['input_ids']
+        summary_encoding = tokenizer(examples[data_args.summary_column], max_length=data_args.max_target_length, padding=padding, truncation=True, is_split_into_words=False)['input_ids']
         # concatenate
         edited_sents = []
         for t, s in zip(text_encoding, summary_encoding):
             sent = t + [separator] + s + [eos_idx]
             edited_sents.append(sent)
 
-        # mask labels
+        # mask labels as to not train on the input text
         labels = copy.deepcopy(edited_sents)
         for i,l in enumerate(labels):
             sep_idx = l.index(separator) + 1
@@ -200,25 +221,137 @@ def finetune():
             "labels": labels
         }
     
+    if training_args.do_train:
+        train_dataset = raw_datasets['train']
+        if data_args.max_train_samples is not None:
+            max_train_samples = min(len(train_dataset), data_args.max_train_samples)
+            train_dataset = train_dataset.select(range(max_train_samples))
+        with training_args.main_process_first(desc="train dataset map pre-processing"):
+            train_dataset = train_dataset.map(
+                preprocess_function,
+                batched=True,
+                num_proc=data_args.preprocessing_num_workers,
+                remove_columns=train_dataset.column_names,
+                load_from_cache_file=not data_args.overwrite_cache,
+                desc="Running tokenizer on train dataset",
+            )
 
-    train_dataset = raw_datasets['train'].map(preprocess_function, batched=True, remove_columns=raw_datasets['train'].column_names)
-    eval_dataset = raw_datasets['validation'].map(preprocess_function, batched=True, remove_columns=raw_datasets['validation'].column_names)
+    if training_args.do_eval:
+        eval_dataset = raw_datasets['eval']
+        if data_args.max_eval_samples is not None:
+            max_eval_samples = min(len(eval_dataset), data_args.max_eval_samples)
+            eval_dataset = eval_dataset.select(range(max_eval_samples))
+        with training_args.main_process_first(desc="eval dataset map pre-processing"):
+            eval_dataset = eval_dataset.map(
+                preprocess_function,
+                batched=True,
+                num_proc=data_args.preprocessing_num_workers,
+                remove_columns=eval_dataset.column_names,
+                load_from_cache_file=not data_args.overwrite_cache,
+                desc="Running tokenizer on eval dataset",
+            )
+
+    if training_args.do_predict:
+        predict_dataset = raw_datasets['predict']
+        if data_args.max_predict_samples is not None:
+            max_predict_samples = min(len(predict_dataset), data_args.max_predict_samples)
+            predict_dataset = predict_dataset.select(range(max_predict_samples))
+        with training_args.main_process_first(desc="predict dataset map pre-processing"):
+            predict_dataset = predict_dataset.map(
+                preprocess_function,
+                batched=True,
+                num_proc=data_args.preprocessing_num_workers,
+                remove_columns=predict_dataset.column_names,
+                load_from_cache_file=not data_args.overwrite_cache,
+                desc="Running tokenizer on predict dataset",
+            )
+
     
     # set up trainer
     trainer = Trainer(
         model=model,
         args=training_args,
-        train_dataset=train_dataset,
-        eval_dataset=eval_dataset,
+        train_dataset=train_dataset if training_args.do_train else None,
+        eval_dataset=eval_dataset if training_args.do_eval else None,
         tokenizer=tokenizer,
         # data_collator=data_collator
     )
+
     # launch fine tuning
-    #trainer.train(resume_from_checkpoint=f"{model_args.model_name_or_path}")
-    trainer.train()
-    # save final model
-    trainer.save_model()
-    trainer.save_state()
+    if training_args.do_train:
+        checkpoint = None
+        if training_args.resume_from_checkpoint is not None:
+            checkpoint = training_args.resume_from_checkpoint
+        elif last_checkpoint is not None:
+            checkpoint = last_checkpoint
+        train_result = trainer.train(resume_from_checkpoint=checkpoint)
+        trainer.save_model()  # Saves the tokenizer too for easy upload
+
+        metrics = train_result.metrics
+        max_train_samples = (
+            data_args.max_train_samples if data_args.max_train_samples is not None else len(train_dataset)
+        )
+        metrics["train_samples"] = min(max_train_samples, len(train_dataset))
+
+        trainer.log_metrics("train", metrics)
+        trainer.save_metrics("train", metrics)
+        trainer.save_state()
+    
+    # Evaluation
+    results = {}
+    if training_args.do_eval:
+        logger.info("*** Evaluate ***")
+        metrics = trainer.evaluate(metric_key_prefix="eval")
+        max_eval_samples = data_args.max_eval_samples if data_args.max_eval_samples is not None else len(eval_dataset)
+        metrics["eval_samples"] = min(max_eval_samples, len(eval_dataset))
+
+        trainer.log_metrics("eval", metrics)
+        trainer.save_metrics("eval", metrics)
+
+    if training_args.do_predict:
+        logger.info("*** Predict ***")
+
+        # predict_results = trainer.predict(predict_dataset, metric_key_prefix="predict", **gen_kwargs)
+        predict_results = trainer.predict(predict_dataset, metric_key_prefix="predict")
+        metrics = predict_results.metrics
+        max_predict_samples = (
+            data_args.max_predict_samples if data_args.max_predict_samples is not None else len(predict_dataset)
+        )
+        metrics["predict_samples"] = min(max_predict_samples, len(predict_dataset))
+
+        trainer.log_metrics("predict", metrics)
+        trainer.save_metrics("predict", metrics)
+
+        if trainer.is_world_process_zero():
+            if training_args.predict_with_generate:
+                predictions = predict_results.predictions
+                predictions = np.where(predictions != -100, predictions, tokenizer.pad_token_id)
+                predictions = tokenizer.batch_decode(
+                    predictions, skip_special_tokens=True, clean_up_tokenization_spaces=True
+                )
+                predictions = [pred.strip() for pred in predictions]
+                output_prediction_file = os.path.join(training_args.output_dir, "generated_predictions.txt")
+                with open(output_prediction_file, "w") as writer:
+                    writer.write("\n".join(predictions))
+
+    kwargs = {"finetuned_from": model_args.model_name_or_path, "tasks": "summarization"}
+    if data_args.dataset_name is not None:
+        kwargs["dataset_tags"] = data_args.dataset_name
+        if data_args.dataset_config_name is not None:
+            kwargs["dataset_args"] = data_args.dataset_config_name
+            kwargs["dataset"] = f"{data_args.dataset_name} {data_args.dataset_config_name}"
+        else:
+            kwargs["dataset"] = data_args.dataset_name
+
+    if data_args.lang is not None:
+        kwargs["language"] = data_args.lang
+
+    if training_args.push_to_hub:
+        trainer.push_to_hub(**kwargs)
+    else:
+        trainer.create_model_card(**kwargs)
+
+    return results
 
 
 if __name__ == "__main__":
